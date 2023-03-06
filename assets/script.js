7020206fb13b5b2ae0de2ca134db76ec51297852
goldie <codegayle@gmail.com>
2023-03-05 22:13:34 -0500

    fixed image icons.

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,12 @@ var lat
 var long
 var weatherCondition
 var date = " " + dayjs().format('dddd, MMMM D YYYY')
-var locationNamesa
+var locationNamesa =[]
+var searchState 
+var listItem
+var getHistory
+var searchHistory
+var topNine 
 
 //
 // On load, page has buttons to access past searches. This function returns the most recent nine searches.
@@ -18,18 +23,22 @@ searchHistory = topNine.reverse();
 
 
 $(document).ready(function () {
+ getHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+ searchHistory = getHistory.reverse();
+ topNine = searchHistory.splice(0, 8);
+searchHistory = topNine.reverse();
   $(".search").append("<ul class=pastSearch>");
-  var locationNamesa = [];
+  //var locationNamesa = [];
   for (i = 0; i < searchHistory.length; i++) {
     locationNamesa.push(searchHistory[i].Location)
     $(".pastSearch").prepend("<li class=pastItem>")
     var listItem = document.querySelector(".pastItem")
     var locName = searchHistory[i].Location;
-    listItem.textContent = locName;
+    searchState =searchHistory[i].State;
+    listItem.textContent = locName + " "+ searchState;
     var locNameRefine = locName.replace(/ /g, "%20");
     listItem.classList.add(locNameRefine);
   }
-
   var locations = $('.pastItem')
   for (i = 0; i < locations.length; i++) {
     locations[i].addEventListener('click', function () {
@@ -37,20 +46,52 @@ $(document).ready(function () {
       $('.current').html("")
       $(this).removeClass("pastItem");
       var classLoc = $(this).attr("class");
+      $(this).addClass("pastItem");
       var classLocRefine = classLoc.replace(/%20/g, " ")
       var index = locationNamesa.indexOf(classLocRefine)
       var pastObject = searchHistory[index];
       lat = pastObject.Latitude
       long = pastObject.Longitude
+      searchState = pastObject.State
+     
+      
+      getForeCast(lat, long);
+      getWeather(lat, long);
+    })
+    }})
+
+    
+/*function addEventListeners () {
+
+getHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+ searchHistory = getHistory.reverse();
+ topNine = searchHistory.splice(0, 8);
+searchHistory = topNine.reverse();
+  var locations = $('.pastItem')
+  for (i = 0; i < locations.length; i++) {
+    locations[i].addEventListener('click', function () {
+      $('.foreCard').html("")
+      $('.current').html("")
+      $(this).removeClass("pastItem");
+      var classLoc = $(this).attr("class");
+      $(this).addClass("pastItem");
+      var classLocRefine = classLoc.replace(/%20/g, " ")
+      var index = locationNamesa.indexOf(classLocRefine)
+      var pastObject = searchHistory[index];
+      console.log(pastObject)
+      lat = pastObject.Latitude
+      long = pastObject.Longitude
+      searchState = pastObject.State
+     
       
       getForeCast(lat, long);
       getWeather(lat, long);
 
-    })
+    })}
+  
+
   }
-})
-
-
+*/
 //This function displays current weather conditions based on latitude and longitude
 function getWeather(lat, long) {
   var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=6cdac48da9c3705ea4ff93e00c557ffc"
@@ -63,9 +104,10 @@ function getWeather(lat, long) {
       return res.json();
     })
     .then(data => {
+      
       $('.current').append("<h3 class=location>");
       var locHeading = document.querySelector(".location");
-      locHeading.textContent = data.name;
+      locHeading.textContent = data.name + " "+searchState;
       $('.current').append("<h5 class=date>");
       var locHeading = document.querySelector(".date");
       locHeading.textContent = date;
@@ -115,19 +157,27 @@ function getForeCast() {
       var foreCastRefine = [foreCastFive[indexBegin], foreCastFive[indexBegin + interval], foreCastFive[indexBegin + (2 * interval)], foreCastFive[indexBegin + (3 * interval)], foreCastFive[indexBegin + (4 * interval)]]
       foreCastRefine.forEach(dayFore => {
        var weatherDay =$('<div class=weatherDay>');
+       weatherDay.addClass("card text-white bg-primary mb-3 col-2")
       $('.foreCard').append(weatherDay)
         
         var dateForeCast = $('<h5>');
-        dateForeCast.text(dayjs(dayFore.dt_txt).format('dddd, MMMM D YYYY'));
+        dateForeCast.text(dayjs(dayFore.dt_txt).format('dddd, MMMM D'));
         $("div.weatherDay:last-of-type").append(dateForeCast)
-        $("div.weatherDay:last-of-type").append("<h4 class=mainWeather>");
-        var locHeading = document.querySelector(".mainWeather");
-        locHeading.textContent = dayFore.weather[0].main;
-        $("div.weatherDay:last-of-type").append("<img class=icon>");
-        var locHeading = document.querySelector(".icon");
+        var mainWeatherFore = $('<h4>');
+        mainWeatherFore.text(dayFore.weather[0].main);
+        $("div.weatherDay:last-of-type").append(mainWeatherFore);
+        
+       //is updating all of the icon's to the last one
+       var imageIcon = $('<img>') 
+        
         var iconcode = dayFore.weather[0].icon
+        console.log(iconcode)
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"
-        $('.icon').attr('src', iconurl)
+        console.log(iconurl)
+        
+        $("div.weatherDay:last-of-type").append(imageIcon);
+        imageIcon.attr("src",iconurl)
+        
         var currentTemp = $('<h5>')
         currentTemp.text("Temperature: " + ((dayFore.main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "\u{00B0}F")
         $("div.weatherDay:last-of-type").append(currentTemp)
@@ -155,10 +205,14 @@ $("#search-btn").on("click", function () {
       $('.current').html("")
   var presearchCity = $(".search-input").val();
   var searchCity = presearchCity.trim().replace(/ /g, "%20")
-  var searchState = $(".form-control").children("option:selected").val();
+  searchState = $(".form-control").children("option:selected").val();
+  if (searchCity === ""){
+    alert("please enter a value to get weather")
+    return;
+  }
   var apiQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchCity + "," + searchState + ",+1&limit=1&appid=6cdac48da9c3705ea4ff93e00c557ffc"
   searchApi();
-  function searchApi(searchCity, searchState) {
+  function searchApi() {
     fetch(apiQueryURL)
       .then(res => {
         if (!res.ok) {
@@ -174,8 +228,8 @@ $("#search-btn").on("click", function () {
           lat = location.lat.toFixed(2);
           long = location.lon.toFixed(2);
           $(".pastSearch").prepend("<li class=pastItem>")
-          var listItem = document.querySelector(".pastItem")
-          var locNameb = location.name;
+          listItem = document.querySelector(".pastItem")
+          var locNameb = location.name +" " +searchState;
           listItem.textContent = locNameb;
           var locNamebRefine = locNameb.replace(/ /g, "%20")
           listItem.classList.add(locNamebRefine);
@@ -184,11 +238,12 @@ $("#search-btn").on("click", function () {
             locationNamesb.push(searchHistory[i].Location)
           }
           if (locationNamesb.indexOf(location.name) === -1) {
-            searchHistory.push({ Location: location.name, Latitude: lat, Longitude: long })
+            searchHistory.push({ Location: location.name, Latitude: lat, Longitude: long, State: searchState })
           };
           localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-          getForeCast(lat, long);
+          getForeCast();
           getWeather(lat, long);
+          
 
 
         })
@@ -196,9 +251,26 @@ $("#search-btn").on("click", function () {
 
       })
 
-  }
-})
+  }})
 
+ /* $(".pastItem").on('click', function () {
+    $('.foreCard').html("")
+    $('.current').html("")
+    $(this).removeClass("pastItem");
+    var classLoc = $(this).attr("class");
+    $(this).addClass("pastItem");
+    var classLocRefine = classLoc.replace(/%20/g, " ")
+    var newHistory = JSON.parse(localStorage.getItem("searchHistory"))
+    var index = newHistory.indexOf(classLocRefine)
+    console.log(index)
+    var pastObject = newHistory[index];
+    lat = pastObject.Latitude
+    long = pastObject.Longitude
+    searchState = pastObject.State
+   
+    
+    getForeCast(lat, long);
+    getWeather(lat, long);})
 
 
 
@@ -211,4 +283,4 @@ $("#search-btn").on("click", function () {
 
      //Code source for image: https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
      //Code source for state list: https://gist.github.com/RichLogan/9903043
-
+*/
